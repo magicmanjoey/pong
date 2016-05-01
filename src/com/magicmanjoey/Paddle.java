21a2ae3db022c8681567f15e7a866ab3df964f35
Joey Steele <magicmanjoey@comcast.net>
2016-04-30 21:28:44 -0400

    Add Paddle class

--- src/com/magicmanjoey/Paddle.java
+++ src/com/magicmanjoey/Paddle.java
@@ -1,3 +1,48 @@
 public class Paddle {
+	//Paddle's height
+	private static final int HEIGHT = 30;
 	
+	private static final double PADDLE_SPEED = 5;
+	
+	//READ BOARD HEIGHT FROM BOARD CLASS WHEN IT EXISTS!!!!!!
+	//Board max height (board's actual height is this * 2)
+	private static final int BOARD_HEIGHT = 100;
+	
+	//Paddle's y-position
+	private int posY;
+	
+	private Player.PlayerSide player;
+	
+	public Paddle(Player.PlayerSide p) {
+		player = p;
+		posY = 0;
+	}
+	
+	public void moveUp() {
+		if((posY + PADDLE_SPEED + (HEIGHT/2)) > BOARD_HEIGHT) {
+			posY = BOARD_HEIGHT - HEIGHT/2;
+		} else {
+			posY += PADDLE_SPEED;
+		}
+	}
+	
+	public void moveDown() {
+		if((posY - PADDLE_SPEED - (HEIGHT/2)) < -BOARD_HEIGHT) {
+			posY = -BOARD_HEIGHT + HEIGHT/2;
+		} else {
+			posY -= PADDLE_SPEED;
+		}
+	}
+	
+	public int getPosY() {
+		return posY;
+	}
+	
+	public int getHeight() {
+		return HEIGHT;
+	}
+	
+	public Player.PlayerSide getPlayer() {
+		return player;
+	}
 }
---
+++ src/com/magicmanjoey/Player.java
@@ -0,0 +1,5 @@
+public class Player {
+
+	public enum PlayerSide {LEFT, RIGHT};
+	
+}
