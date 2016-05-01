347477666ecf56ad5dc50055129452b60f6cd68f
Joey Steele <magicmanjoey@comcast.net>
2016-05-01 09:01:27 -0400

    Complete Player class

--- src/com/magicmanjoey/Player.java
+++ src/com/magicmanjoey/Player.java
@@ -4,5 +4,28 @@ public class Player {
 	
 	private int score;
 	private Paddle paddle;
+	private PlayerSide side;
 	
+	public Player(PlayerSide side, Paddle paddle) {
+		this.side = side;
+		this.paddle = paddle;
+	}
+	
+	public void incrementScore() {
+		score++;
+	}
+	
+	public void setScore(int score) {
+		if(score>=0) {
+			this.score = score;
+		}
+	}
+
+	public PlayerSide getPlayerSide() {
+		return side;
+	}
+
+	public int getScore() {
+		return score;
+	}
 }
