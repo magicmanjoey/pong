038450130a7f0b5c189b3deac84414f62af5fdd4
Joey Steele <magicmanjoey@comcast.net>
2016-04-30 21:44:20 -0400

    Make random() and bounceAngle() in Calc.java static

--- src/com/magicmanjoey/Calc.java
+++ src/com/magicmanjoey/Calc.java
@@ -3,12 +3,12 @@ public class Calc {
 	public Calc() {}
 	
 	//Returns random int between two args
-	public int random(int min, int max) {
+	public static int random(int min, int max) {
 		return (int) (Math.random() * (max-min) + min);
 	}
 	
 	//Calculates "perfect" angle of reflection for ball bounce
-	public int bounceAngle(int angle1) {
+	public static int bounceAngle(int angle1) {
 		return 360-angle1;
 	}
 	
