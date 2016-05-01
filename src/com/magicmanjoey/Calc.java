6e16f5c6e02fa02be36f50bceda02eb8d3fc3cb1
Joey Steele <magicmanjoey@comcast.net>
2016-05-01 18:40:26 -0400

    Add mapping methods to Calc.java for conversion of coords to pixels

--- src/com/magicmanjoey/Calc.java
+++ src/com/magicmanjoey/Calc.java
@@ -11,5 +11,19 @@ public class Calc {
 	public static int bounceAngle(int angle1) {
 		return 360-angle1;
 	}
+
+	//Maps value1 between low1 and high1 to value between low2 and high2
+	public static int map(int low1, int high1, int low2, int high2, int value1) {
+		return (value1 * ((high2 - low2)/(high1 - low1)));
+	}
 	
+	//Converts x-coordinate to the appropriate pixel value
+	public static int xCoordToPixel(int xCoord, int windowWidth, int xPixelMargin, int boardWidth) {
+		return (map(-boardWidth, boardWidth, 0, (windowWidth - 2*xPixelMargin), xCoord) + xPixelMargin);
+	}
+	
+	//Converts y-coordinate to the appropriate pixel value
+	public static int yCoordToPixel(int yCoord, int windowHeight, int boardHeight) {
+		return (map(boardHeight, -boardHeight, 0, windowHeight, yCoord));
+	}
 }
